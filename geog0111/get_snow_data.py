8d46391e74d10fbed3092dbfb6601acb42a0b89f
profLewis <dr.p.e.lewis@gmail.com>
2020-09-30 15:47:18 +0100

    updated from /nfs/cfs/home3/Uucfa6/ucfalew/geog0111:t

---
+++ geog0111/get_snow_data.py
@@ -0,0 +1,28 @@
+from geog0111.modis_annual import modis_annual
+import numpy as np
+
+def get_snow_data(year):
+    '''
+    '''
+    # load some data
+    sds     = ['NDSI_Snow_Cover']
+    product = 'MOD10A1'
+    tile = ['h09v05']
+    warp_args = {
+      'dstNodata'     : 255,
+      'format'        : 'MEM',
+      'cropToCutline' : True,
+      'cutlineWhere'  : f"'HUC=13010001'",
+      'cutlineDSName' : 'data/Hydrological_Units/HUC_Polygons.shp'
+    }
+    
+    mfiles = modis_annual(year,tile,product,verbose=True,\
+                          sds=sds,warp_args=warp_args)
+    # scale it
+    # doy from filenames
+    doy = np.array([int(i.split('-')[1]) for i in mfiles['bandnames']])
+    print(mfiles[sds[0]],doy)
+    return mfiles[sds[0]],doy
+
+get_snow_data(2019)
+
