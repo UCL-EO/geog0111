f6b3ba89273f715c0c4562b844d9d6effd8b983e
profLewis <dr.p.e.lewis@gmail.com>
2020-11-02 13:08:43 +0000

    updated from /nfs/cfs/home3/Uucfa6/ucfalew/geog0111:t

--- notebooks/geog0111/get_lc.py
+++ notebooks/geog0111/get_lc.py
@@ -12,6 +12,7 @@ geog0111/get_lc.py
 
 at the command prompt.
 '''
+import numpy as np
 
 import gdal
 try:
@@ -20,13 +21,40 @@ except:
   from modis import Modis
 import matplotlib.pyplot as plt
 
-for tile in [['h17v03','h18v03','h17v04','h18v04'],[h22v10']]:
-  kwargs = {
-    'verbose' : True,
-    'tile'      :    tile,
-    'product'   :    'MCD12Q1',
-  }
-  for year in [2017,2018,2019]:
+def get_lc(year,tile,fips):
+    '''
+    Return LC mask for year,tile,fips
+    '''
+    kwargs = {
+        'tile'      :    tile,
+        'product'   :    'MCD12Q1',
+    }
+    doy = 1
+    # get the LC data
     modis = Modis(**kwargs)
-    mfiles = modis.get_modis(year,[1])
-    print(mfiles.keys())
+
+    warp_args = {
+      'dstNodata'     : 255,
+      'format'        : 'MEM',
+      'cropToCutline' : True,
+      'cutlineWhere'  : f"FIPS='{fips}'",
+      'cutlineDSName' : 'data/TM_WORLD_BORDERS-0.3.shp'
+    }
+
+    # specify day of year (DOY) and year
+    lcfiles = modis.get_modis(year,doy,warp_args=warp_args)
+    # get the item we want
+    g = gdal.Open(lcfiles['LC_Type3'])
+    # error checking
+    if not g:
+        print(f"cannot open LC file {lcfiles['LC_Type3']}")
+        return None
+    lc = g.ReadAsArray()
+    del g
+    
+    # in your function, print out the unique values in the 
+    # landcover dataset to give some feedback to the user
+    print(f"class codes: {np.unique(lc)}")
+    return lc
+
+
