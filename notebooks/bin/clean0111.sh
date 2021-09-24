77af64f1756a78e71a14eba9aebe07d393b7d6bb
Professor Philip Lewis <dr.p.e.lewis@gmail.com>
2021-09-24 09:56:36 +0100

    Update clean0111.sh
    make a backup before cleaning

--- notebooks/bin/clean0111.sh
+++ notebooks/bin/clean0111.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# clean up all settings and files for a user for geog0111
+
+echo "Clean up script for geog0111"
+echo "This will clean all settings for users"
+echo "beware of unintended consequences"
+DATE=`date '+%d%m%Y_%H%M%S'`
+echo "A backup of all relevant files is made to ~/BACKUP-$DATE-geog0111.tar.Z"
+
 cd ~
+touch geog0111 .url_db .cylog .jupyter .condarc .profile .ipython .bash_profile .bashrc
+tar cvzf ~/BACKUP-$DATE-geog0111.tar.Z geog0111 .url_db .cylog .jupyter .condarc .profile .ipython .bash_profile .bashrc
+
 rm -rf geog0111/notebooks/bin
 rm -rf geog0111
+
+# clean any changes to bash_profile
+if [ -f ~/.bash_profile ]; then
+    echo "~/.bash_profile exists... editing out reference to ~/.profile"
+    grep -v < ~/.bash_profile 'source ~/.profile' > /tmp/tmp.$$ ; mv /tmp/tmp.$$  ~/.bash_profile
+
+fi
 rm -rf .url_db .cylog .jupyter .condarc .profile .ipython
+
+echo "You should manually edit ~/.bashrc to remove conda settings for a full reset"
 
 
